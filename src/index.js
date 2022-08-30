3fcc65c63dde05ee9c6f4e243027d729a199aaff
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2022-08-30 15:37:12 -0400

    changed comments for clarity

--- src/index.js
+++ src/index.js
@@ -65,8 +65,8 @@ const init = () => {
     shell.exec(`mkdir -p ${outputPath.replace(/[^\/]*$/, '')}`);
   }
 
-  /* exeption for windows machines removes strings that are necesary on unix systems for echo command 
-    but meanwhile wrap output css in strings on windows 
+  /* exeption for windows machines removes strings from echo command that are necesary on unix systems for echo command 
+    but meanwhile wrap output css in strings on windows. The solution removes the strings on windows but keeps them on unix systems
   */
   if (process.platform === "win32") {
     shell.exec(`echo ${css} > ${outputPath}`);
